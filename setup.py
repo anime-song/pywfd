70a1835ab7c95c6835277ecbf940a7423a2a4e41
anime-song <op_2016_11_16@yahoo.co.jp>
2020-03-06 05:51:47 +0900

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pywfd",
-    version="1.1.3",
+    version="1.1.4",
     description="A library to handle wfd in python.",
     install_requires=["numpy", "pandas", "dlchord"],
     author="anime-song",
